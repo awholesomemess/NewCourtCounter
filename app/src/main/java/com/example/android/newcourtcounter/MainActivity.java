575e0fa32368b9deae708dd949c3940994869def
awholesomemess <awholesomemess@gmail.com>
2018-02-20 00:51:00 -0500

    New Commit, working on adding button functionality

--- app/src/main/java/com/example/android/newcourtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/newcourtcounter/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.android.newcourtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int score =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.score);
         scoreView.setText(String.valueOf(score));
+    }
+    public void threePointer (View view){
+        displayForTeamA(score+3);
     }
 }
